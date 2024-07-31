16e284395ef787d172758b40cc0130027a674b0a
B-pear <11268651@qq.com>
2024-07-31 13:53:27 +0800

    修改部分翻译文件

--- src/libDialog/Translations/LibDialog_zh_CN.ts
+++ src/libDialog/Translations/LibDialog_zh_CN.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
+﻿<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
@@ -6,27 +6,27 @@
     <message>
         <location filename="../dialogabout.ui" line="14"/>
         <source>关于“SecondDownloader”</source>
-        <translation type="unfinished"></translation>
+        <translation>关于“SecondDownloader”</translation>
     </message>
     <message>
         <location filename="../dialogabout.ui" line="99"/>
         <source>SecondDownloader V1.0.0.0</source>
-        <translation type="unfinished"></translation>
+        <translation>SecondDownloader V1.0.0.0</translation>
     </message>
     <message>
         <location filename="../dialogabout.ui" line="119"/>
         <source>Developed by Pinsoft Studio</source>
-        <translation type="unfinished"></translation>
+        <translation>Developed by Pinsoft Studio</translation>
     </message>
     <message>
         <location filename="../dialogabout.ui" line="153"/>
         <source>Github(&amp;G)</source>
-        <translation type="unfinished"></translation>
+        <translation>Github(&amp;G)</translation>
     </message>
     <message>
         <location filename="../dialogabout.ui" line="179"/>
         <source>确定(&amp;O)</source>
-        <translation type="unfinished"></translation>
+        <translation>确定(&amp;O)</translation>
     </message>
 </context>
 <context>
@@ -34,27 +34,27 @@
     <message>
         <location filename="../dialogcrtinf.ui" line="19"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Dialog</translation>
     </message>
     <message>
         <location filename="../dialogcrtinf.ui" line="51"/>
         <source>确定</source>
-        <translation type="unfinished"></translation>
+        <translation>确定</translation>
     </message>
     <message>
         <location filename="../dialogcrtinf.ui" line="81"/>
         <source>取消</source>
-        <translation type="unfinished"></translation>
+        <translation>取消</translation>
     </message>
     <message>
         <location filename="../dialogcrtinf.ui" line="111"/>
         <source>记事本</source>
-        <translation type="unfinished"></translation>
+        <translation>记事本</translation>
     </message>
     <message>
         <location filename="../dialogcrtinf.ui" line="129"/>
         <source>文件已经打开。</source>
-        <translation type="unfinished"></translation>
+        <translation>文件已经打开。</translation>
     </message>
 </context>
 <context>
@@ -62,27 +62,27 @@
     <message>
         <location filename="../dialogquestion.ui" line="19"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Dialog</translation>
     </message>
     <message>
         <location filename="../dialogquestion.ui" line="51"/>
         <source>是</source>
-        <translation type="unfinished"></translation>
+        <translation>是</translation>
     </message>
     <message>
         <location filename="../dialogquestion.ui" line="81"/>
         <source>否</source>
-        <translation type="unfinished"></translation>
+        <translation>否</translation>
     </message>
     <message>
         <location filename="../dialogquestion.ui" line="111"/>
         <source>记事本</source>
-        <translation type="unfinished"></translation>
+        <translation>记事本</translation>
     </message>
     <message>
         <location filename="../dialogquestion.ui" line="129"/>
         <source>是否要将更改保存到“未命名.txt”？</source>
-        <translation type="unfinished"></translation>
+        <translation>是否要将更改保存到“未命名.txt”？</translation>
     </message>
 </context>
 </TS>
